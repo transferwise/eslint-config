df74a6791cadb95796238ce631fb82396aa2fdef
mikk.narusk <mikk.narusk@transferwise.com>
2019-05-06 14:32:09 +0300

    Add exception for react destructuring

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
   rules: {
     'prettier/prettier': ['error', prettierConfig],
+    'react/destructuring-assignment': [2, 'always', { ignoreClassFields: true }],
     'react/jsx-filename-extension': 0,
     'no-use-before-define': 0,
     'jsx-a11y/label-has-for': [
