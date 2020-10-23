3b5b1677202b1e819967a8c18c903a674d96a93e
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-10-23 11:36:48 +0300

    Use TypeScript, add plugins and rules, update ESLint and other dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,99 @@
-const prettierConfig = require('./.prettierrc');
-
 module.exports = {
-  parser: 'babel-eslint',
-
-  env: {
-    browser: true,
-    jest: true,
-  },
+  extends: [
+    'eslint:recommended',
+    // TODO: Consider more rules from https://eslint.org/docs/rules/
+    'react-app',
+    'plugin:jsx-a11y/strict',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'plugin:jest/all',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/react',
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended',
+    // recommended exclusions, explained in https://github.com/prettier/eslint-config-prettier/blob/master/README.md#installation
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'prettier/unicorn',
+  ],
   plugins: ['fp'],
-  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  // for plugin:@typescript-eslint/recommended-requiring-type-checking, explained in https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/TYPED_LINTING.md
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
   rules: {
-    'react/jsx-props-no-spreading': [
-      2,
+    'import/order': [
+      'error',
       {
-        explicitSpread: 'ignore',
-        custom: 'ignore',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
       },
     ],
-    'prettier/prettier': ['error', prettierConfig],
-    'react/destructuring-assignment': [2, 'always', { ignoreClassFields: true }],
-    'react/jsx-filename-extension': 0,
-    'no-use-before-define': 0,
-    'jsx-a11y/label-has-for': 0,
-    'jsx-a11y/label-has-associated-control': [
-      2,
+    'jest/valid-title': ['error', { ignoreTypeOfDescribeName: true }], // to support using a function as a title, which prevents titles from getting out of date
+    'jest/prefer-expect-assertions': 'off', // too verbose for every test
+    'jest/no-hooks': 'off', // we're using them for setup, up for consideration
+    'jest/lowercase-name': [
+      'error',
       {
-        assert: 'either',
+        ignore: ['describe', 'test'], // these are valid in sentence case
       },
     ],
-    'import/prefer-default-export': 0,
-    'prefer-promise-reject-errors': 0,
+    curly: 'error',
+    'testing-library/no-manual-cleanup': 'error',
+    'testing-library/no-render-in-setup': 'error',
+    'testing-library/no-wait-for-empty-callback': 'error',
+    'testing-library/no-wait-for-snapshot': 'error',
+    'testing-library/prefer-explicit-assert': 'error',
+    'testing-library/prefer-presence-queries': 'error',
+    'testing-library/prefer-screen-queries': 'error',
+    'testing-library/prefer-wait-for': 'error',
+    'unicorn/no-null': 'off', // a bit too ambitious
+    'unicorn/filename-case': 'off', // too many exceptions required, for [] in Next.js etc.
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          req: false,
+          res: false,
+          env: false,
+          props: false,
+          param: false,
+          params: false,
+          pkg: false,
+        },
+      },
+    ],
+    'unicorn/no-fn-reference-in-iterator': 'off', // mitigated by TypeScript and reduces readability, up for consideration
+    'unicorn/prefer-number-properties': 'off', // no IE support, up for consideration
+    'unicorn/no-reduce': 'off', // we use it a lot and believe the lack of mutation is acceptable
+    'unicorn/no-useless-undefined': 'off', // sometimes useful to be explicit
+    'unicorn/better-regex': 'off', // sometimes too smart, resulting in expressions like "Ac{2}ount number"
+    'unicorn/no-unsafe-regex': 'error',
+    'unicorn/no-unused-properties': 'error',
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'enzyme',
+        message: 'Use @testing-library/react instead',
+      },
+    ],
+    'prefer-object-spread': 'error',
     'fp/no-mutation': [
       'error',
       {
         commonjs: true,
         allowThis: true,
-        exceptions: [
-          { property: 'propTypes' },
-          { property: 'defaultProps' },
-          { property: 'getInitialProps' },
-          { object: 'window' },
-        ],
+        exceptions: [{ property: 'getInitialProps' }, { object: 'window' }],
       },
     ],
-    'fp/no-mutating-assign': 'error',
-    curly: [1, 'all'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}, // for ESLint to know about tsconfig.json baseUrl and distinguish external and internal imports in import/order
+    },
   },
 };
